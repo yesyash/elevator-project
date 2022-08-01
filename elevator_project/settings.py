8f47bb94678965528abb0a1b5cf74169d48d236b
Yash Raj <yashraj0207@outlook.com>
2022-08-01 10:20:46 +0530

    changed allowed hosts

--- elevator_project/settings.py
+++ elevator_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-b@y^1s900yq1=is=y!!iqnhkz4p!z)i_bh3p55ke5(f!wvsky&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'elevators-project.herokuapp.com']
 
 
 # Application definition
